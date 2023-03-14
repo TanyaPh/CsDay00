7cacb7d0775b6f36779c13db4b93cc287aa9f469
Gabriela Lupita <gabriela@pr-n3.kzn.21-school.ru>
2023-03-14 21:58:16 +0300

    d_01

---
+++ src/d00/d00_ex00/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+if (args.Length != 3 || !double.TryParse(args[0], out var sum) ||
+    !double.TryParse(args[1], out var rate) ||
+    !int.TryParse(args[2], out var term))
+{
+    Console.WriteLine("Something went wrong. Check your input and retry.");
+    return;
+}
+
+var i = rate / 12.0 / 100.0;
+var p = Math.Pow((1+i), term);
+var payment = (sum * i * p) / (p-1);
+
+var currentDate = DateTime.Today;
+// var currentDate = new DateTime(2021, 5, 1);
+// var currentDate = new DateTime(2021, 5, 1);
+var debt = sum;
+
+for (var countMonths = 1; countMonths <= term; countMonths++)
+{
+    var period = (int) (currentDate.AddMonths(countMonths) - currentDate.AddMonths(countMonths - 1)).TotalDays;
+    var interest = (debt * rate * period) / (100 * (Convert.ToInt16(DateTime.IsLeapYear(currentDate.Year)) + 365));
+    var principalDebt = payment - interest;
+    debt -= principalDebt;
+    if (countMonths == term)
+    {
+        payment += debt;
+        principalDebt = payment - interest;
+        debt = 0;
+    }
+    Console.WriteLine($"{countMonths}\t{currentDate.AddMonths(countMonths):d}\t{payment:N2}\t{principalDebt:N2}\t{interest, -8:N2}\t{debt:N2}");
+}
+
---
+++ src/d00/d00_ex01/Program.cs
@@ -0,0 +1,83 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int LevDist(string word1, string word2)
+{
+    var matrix = new int[word1.Length + 1, word2.Length + 1];
+    for (var i = 0; i < word1.Length; i++)
+    {
+        matrix[i, 0] = i;
+    }
+    for (var j = 0; j < word2.Length; j++)
+    {
+        matrix[0, j] = j;
+    }
+    for (var j = 1; j <= word2.Length; j++)
+    {
+        for (var i = 1; i <= word1.Length; i++)
+        {
+            if (word1[i-1] == word2[j-1])
+            {
+                matrix[i, j] = matrix[i-1, j-1];
+            }
+            else
+            {
+                matrix[i, j] = Math.Min(Math.Min(matrix[i - 1, j] + 1, matrix[i, j - 1] + 1), matrix[i - 1, j - 1] + 1);
+            }
+        }
+    }
+    return matrix[word1.Length, word2.Length];
+}
+
+
+Console.Write(">Enter name: ");
+var name = Console.ReadLine();
+
+if (name.Trim() == string.Empty)
+{
+    Console.Write("Your name was not found.");
+    return;
+}
+
+if (!File.Exists("/Users/gabriela/Downloads/us.txt"))
+{
+    Console.Write("File not found");
+}
+else
+{
+    using (var text = File.OpenText("/Users/gabriela/Downloads/us.txt"))
+    {
+        string str;
+        var variants = new List<string>();
+        while ((str = text.ReadLine()) != null)
+        {
+            var res = LevDist(name.Trim(), str);
+            switch (res)
+            {
+                case 0:
+                    Console.WriteLine($"Hello, {name}!");
+                    return;
+                case < 2:
+                    variants.Add(str);
+                    break;
+            }
+        }
+        while (variants.Count != 0)
+        {
+            Console.WriteLine($">Did you mean “{variants[0]}”? Y/N");
+            var answer = Console.ReadLine();
+            switch (answer)
+            {
+                case "Y":
+                    Console.WriteLine($"Hello, {variants[0]}!");
+                    return;
+                case "N":
+                    variants.RemoveAt(0);
+                    break;
+                default:
+                    Console.WriteLine("Something went wrong. Check your input and retry.");
+                    return;
+            }
+        }
+        Console.WriteLine("Your name was not found.");
+    }
+}
